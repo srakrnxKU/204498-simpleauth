0931a978c409e0c691322315439e949345993408
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-03-20 23:39:04 +0700

    Leaving only SHA256...

--- simple_auth/settings.py
+++ simple_auth/settings.py
@@ -104,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
 ]
 
 
