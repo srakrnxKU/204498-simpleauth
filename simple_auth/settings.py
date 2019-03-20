61544777044a01ac510bb738a94afd6d40addf52
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-03-20 23:33:03 +0700

    What is this shit

--- simple_auth/settings.py
+++ simple_auth/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'simple_auth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'simpleauth-dev',
+        'USER': 'simpleauth-dev',
+        'PASSWORD': 'Vx0jTndMBBIjyO2R',
+        'HOST': 'charles.srakrn.me'
     }
 }
 
