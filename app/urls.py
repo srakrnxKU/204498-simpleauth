84e861cf88a738dc0a990aec080926a543c5400d
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-03-21 00:23:04 +0700

    Authenticate routes

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.authenticate, name='login_page')
+    path('login/', views.login_page, name='login_page')
 ]
--- app/views.py
+++ app/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
 
 def index(request):
-    return HttpResponse("This is the login page")
+    return render(request, "login/login.html")
 
 
 def login_page(request):
     username = request.POST['username']
     password = request.POST['password']
+    print("{} {}".format(username, password))
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
+        return HttpResponse("Yay")
     else:
-        pass
+        return redirect("index")
