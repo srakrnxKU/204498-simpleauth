bf812a49a2afd0acee2d2f9e9a26cb41e5c25c0d
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-03-21 22:44:52 +0700

    Checking form method

--- app/views.py
+++ app/views.py
@@ -12,11 +12,12 @@ def index(request):
 
 
 def login_page(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
     return redirect("index")
 
 
