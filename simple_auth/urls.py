832b3cd6106a21663f1c73828a0346a58195879b
Sirakorn Lamyai <tanersirakorn@gmail.com>
2019-04-01 12:59:40 +0700

    Basic register page

--- simple_auth/urls.py
+++ simple_auth/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include("app.urls")),
+    path('register/', include("register.urls")),
     path('admin/', admin.site.urls),
 ]
