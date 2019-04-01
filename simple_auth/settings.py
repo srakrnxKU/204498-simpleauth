87fc64d497407a079cd6a336dcd054e7289c3a7d
tanersirakorn@gmail.com <Sirakorn Lamyai>
2019-04-01 02:53:50 -0400

    Setting allowed hosts

--- simple_auth/settings.py
+++ simple_auth/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'lab4.netlab.kasetsart.org', 'netsec.srakrn.me']
 
 
 # Application definition
